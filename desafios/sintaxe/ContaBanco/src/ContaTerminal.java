ee834e56354d0cb2b815c0d2f92bf94b118ba8b7
lucaslmp2 <lucaspachecolp2@gmail.com>
2025-03-07 13:38:04 -0300

    Primeiro commit do desafio

---
+++ desafios/sintaxe/ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        Scanner ler = new Scanner(System.in);
+        
+
+    }
+}
