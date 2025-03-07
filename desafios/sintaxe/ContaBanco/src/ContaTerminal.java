d6663a7f33efb12f715f4d4de568547b115abc95
lucaslmp2 <lucaspachecolp2@gmail.com>
2025-03-07 14:40:42 -0300

    Desafio finalizado com sucesso!

--- desafios/sintaxe/ContaBanco/src/ContaTerminal.java
+++ desafios/sintaxe/ContaBanco/src/ContaTerminal.java
@@ -1,8 +1,17 @@
+import java.util.Locale;
 import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        Scanner ler = new Scanner(System.in);
-        
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        System.out.println("Digite o número da agência: ");
+        String agencia = scanner.next();
+        Double conta = Math.random() * agencia.hashCode();
+        System.out.println("Digite seu nome: ");
+        String nomeCliente = scanner.next();
+
+        float saldo = 200.00f;
+
+        System.out.println("Olá " + nomeCliente + " " + "obrigado por criar uma conta em nosso banco, sua agência é: " + agencia + ", conta: " + conta + " e seu saldo: R$" + saldo +" já está disponível.");
 
     }
 }
