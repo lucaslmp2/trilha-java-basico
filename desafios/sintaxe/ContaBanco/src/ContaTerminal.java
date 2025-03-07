dd5190172a65ff50eb25344da7b83d57404da0f1
lucaslmp2 <lucaspachecolp2@gmail.com>
2025-03-07 14:55:42 -0300

    Adicionando comentário ao desafio

--- desafios/sintaxe/ContaBanco/src/ContaTerminal.java
+++ desafios/sintaxe/ContaBanco/src/ContaTerminal.java
@@ -1,15 +1,17 @@
 import java.util.Locale;
 import java.util.Scanner;
+/*este desafio tem como propósito proporcionar o entendimento sobre as práticas
+ de desenvolvimento de sintaxe do java ultilizando um exemplo de criação de conta bancaria*/
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);/*classe Scanner sendo instanciada de biblioteca */
         System.out.println("Digite o número da agência: ");
         String agencia = scanner.next();
-        Double conta = Math.random() * agencia.hashCode();
+        Double conta = Math.random() * agencia.hashCode();/*método aleatório de criação de conta */
         System.out.println("Digite seu nome: ");
         String nomeCliente = scanner.next();
 
-        float saldo = 200.00f;
+        float saldo = 200.00f;/* saldo inicial de criação de conta */
 
         System.out.println("Olá " + nomeCliente + " " + "obrigado por criar uma conta em nosso banco, sua agência é: " + agencia + ", conta: " + conta + " e seu saldo: R$" + saldo +" já está disponível.");
 
